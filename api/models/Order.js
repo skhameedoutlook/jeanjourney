286d115a668550893d608a44d3920ac4ca5fa2e4
Shaik Hameed <skhameed@outlook.com>
2023-04-07 18:45:41 +0530

    Order model update

--- api/models/Order.js
+++ api/models/Order.js
@@ -5,11 +5,11 @@ const OrderSchema = new mongoose.Schema({
 	products: [{
 		productId: { type: String, required: true },
 		quantity: { type: Number, required: true, default: 1 },
-		price: {type: Number, required: true}
+		price: {type: Number, required: true},
 		discount: {type: Number, default: 0},
 	}],
 	address: { type: String, required: true },
-	status: { type: String, default: "pending" }
+	status: { type: String, default: "pending" },
 }, { timestamps: true });
 
 module.exports = mongoose.model("Order", OrderSchema);
