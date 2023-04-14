7c6d92240ad4d0a9e84418f46088c385bb29d0a4
Shaik Hameed <skhameed@outlook.com>
2023-04-14 22:03:59 +0530

    Added bootstrap

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
